8b12c5ba3c1ba6c6cf64e5e0532a219464b325f9
alumnos <u202413142@upc.edu.pe>
2026-04-27 11:17:13 -0500

    feature: initial commmit

---
+++ learning-center/src/i18n.js
@@ -0,0 +1,17 @@
+import es from './locales/es.json'
+import en from './locales/en.json'
+
+import {createI18n} from 'vue-i18n'
+
+const i18n = createI18n({
+        legacy:false,
+        locale: 'en',
+        fallbackLocale: 'en',
+        messages: {
+            en,
+            es
+        }
+    }
+);
+
+export default i18n;
